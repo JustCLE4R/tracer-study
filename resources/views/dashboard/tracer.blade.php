39ef9ebe4c15d40fbeed57647ec0ae0c05b72097
Alvin alvito <alvinparis65@gmail.com>
2024-03-01 19:45:12 +0700

    Form Tracer

--- resources/views/dashboard/tracer.blade.php
+++ resources/views/dashboard/tracer.blade.php
@@ -1,5 +1,26 @@
 @extends('dashboard.layouts.main')
 
 @section('content')
-  <h1>ini adalah halaman tracer </h1>
+<div class="container-fluid pt-4 px-4">
+  <div class="row g-4">
+
+    <div class="col-sm-12 col-xl-12">
+      <div class="bg-light rounded p-4">
+          <div class="row">
+              <div class="col-12">
+                  <span class="h6">Isilah Instrumen Pusat Pengembangan Karir  Mahasiswa Berikut Dibawah Ini Dengan Benar!</span>
+              </div>
+          </div>
+
+          @include('dashboard.layouts.filter')
+          @include('dashboard.layouts.wiraswasta')
+          @include('dashboard.layouts.bekerja')
+          
+
+      
+  </div>
+</div>       
+
+
+
 @endsection
