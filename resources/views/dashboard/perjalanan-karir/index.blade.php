e209cf5f4ebbecdcca3ba7e05bc26b32c7ab669d
Alvin alvito <alvinparis65@gmail.com>
2024-03-17 10:09:26 +0700

    update tampilan perjalanan karir index

--- resources/views/dashboard/perjalanan-karir/index.blade.php
+++ resources/views/dashboard/perjalanan-karir/index.blade.php
@@ -1,28 +1,36 @@
 @extends('dashboard.layouts.main')
 
 @section('content')
-<div id="row" class="row justify-content-center p-3">
+<div id="row" class="row justify-content-center ">
 	@if (session()->has('success'))
 	<div class="alert alert-success alert-dismissible fade show col-lg-10 mb-0" role="alert">
 		<strong>Success!</strong> {{ session('success') }}
 		<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
 	</div>
 	@endif
-	<div class="col-lg-5 col-md-7 col-sm-8 col-xs-8 p-2 m-3 position-relative">
+	<div class="col-lg-5 col-md-7 col-sm-8 col-xs-8 my-2  position-relative">
 		<div class="accordion " id="accordionExample">
 			<div class="accordion-item">
 				<h2 class="accordion-header" id="headingOne">
 					<button class="accordion-button border-top border-success border-5 bg-light shadow rounded" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne" style="background-color: #fff;">
-						<span class="h4 text-success p-2" style="font-weight: bold">Riwayat Pekerjaan </span>
+						<span class="h5 text-success p-2" style="font-weight: bold">Riwayat Pekerjaan </span>
 					</button>
 				</h2>
 				<div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
 					<div class="accordion-body">
 						<div class="timeline mx-3 pb-5">
-							<figcaption class="blockquote-footer text-success ms-2">Setelah Lulus </figcaption>
+							<div class="row justify-content-between">
+								
+								<div class="col-7 ">
+									<a href="/dashboard/pekerja/create" class="btn btn-success btn-sm"><i class="bi bi-plus-lg"></i> Tambah Riwayat</a>
+								</div>
+								<div class="col-5 text-end">
+									<span class=" mt-1 text-success ">Setelah Lulus </span>
+								</div>
+							</div>
 							<!--first-->
 							@for ($i = 0; $i < 3; $i++)
-							<div class="timeline__event animated fadeInUp delay-3s timeline__event--type1">
+							<div class="timeline__event animated fadeInUp delay-1s timeline__event--type1">
 								<div class="timeline__event__icon">
 									<i class="bi bi-person-workspace"></i>
 								</div>
@@ -47,33 +55,38 @@
 							@endfor
 						</div>
 	
-						<div class="col position-absolute bottom-0 end-0 mb-3 me-3">
-							<a href="/dashboard/pekerja/create" class="btn btn-success"><i class="bi bi-plus-lg"></i> Tambah Pekerjaan</a>
-						</div>
+						
 					</div>
 				</div>
 			</div>
 		</div>
 	</div>
 	
-	<div class="col-lg-5 col-md-7 col-sm-8 col-xs-8 p-2 m-3 position-relative">
+	<div class="col-lg-5 col-md-7 col-sm-8 col-xs-8 my-2  position-relative">
 		<div class="accordion" id="accordionEducation">
 			<div class="accordion-item">
 				<h2 class="accordion-header" id="headingEducation">
 					<button class="accordion-button border-top border-success border-5 bg-light shadow rounded" type="button" data-bs-toggle="collapse" data-bs-target="#collapseEducation" aria-expanded="true" aria-controls="collapseEducation" style="background-color: #fff;">
-						<span class="h4 text-success p-2" style="font-weight: bold">Riwayat Pendidikan </span>
+						<span class="h5 text-success p-2" style="font-weight: bold">Riwayat Pendidikan </span>
 					</button>
 				</h2>
 				<div id="collapseEducation" class="accordion-collapse collapse show" aria-labelledby="headingEducation" data-bs-parent="#accordionEducation">
 					<div class="accordion-body">
 						<div class="timeline pb-5">
-							<figcaption class="blockquote-footer text-success ms-2">Setelah Lulus </figcaption>
-							<!--first-->
+							<div class="row justify-content-between">
+								
+								<div class="col-7 ">
+									<a href="/dashboard/pendidikan/create" class="btn btn-success btn-sm"><i class="bi bi-plus-lg"></i> Tambah Riwayat</a>
+								</div>
+								<div class="col-5 text-end">
+									<span class=" mt-1 text-success ">Setelah Lulus </span>
+								</div>
+							</div>
 							
 									
 							
 							@foreach ($pendidikans as $pendidikan)
-							<div class="timeline__event animated fadeInUp delay-3s timeline__event--type1">
+							<div class="timeline__event animated fadeInUp delay-1s timeline__event--type1">
 								<div class="timeline__event__icon">
 									<i class="bi bi-mortarboard-fill"></i>
 								</div>
@@ -103,11 +116,10 @@
 							@endforeach
 						</div>
 	
-						<div class="col position-absolute bottom-0 end-0 mb-3 me-3">
-							<a href="/dashboard/pendidikan/create" class="btn btn-success"><i class="bi bi-plus-lg"></i> Tambah Pendidikan</a>
-						</div>
+						
 					</div>
 				</div>
+
 			</div>
 		</div>
 	</div>
