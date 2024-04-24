b2a47b046b1ce7fefd050a72f043e02a4e58b652
Alvin alvito <alvinparis65@gmail.com>
2024-04-24 20:21:59 +0700

    Edit Pekerjaan

--- resources/views/dashboard/perjalanan-karir/kerja/editPekerja.blade.php
+++ resources/views/dashboard/perjalanan-karir/kerja/editPekerja.blade.php
@@ -4,6 +4,12 @@
         <div class="row g-4">
             <div class="col-sm-12 col-xl-12">
                 <div class="bg-light rounded p-5 border-top border-success border-5">
+                    <div class="row">
+                        <div class="col-12">
+                            <span class="h4">Edit Pekerjaan</span>
+                            <hr>
+                        </div>
+                    </div>
                     <form action="/dashboard/perjalanan-karir" method="POST" enctype="multipart/form-data">
 
                         <div class="row justify-content-between" id="dynamicForm">
@@ -50,121 +56,124 @@
                                         lainnya</option>
                                 </select>
                             </div>
-                            <div class="row">
-                                <div class="col-lg-4 col-md-6 col-sm-12 my-2">
-                                    <label class="form-label text-secondary">Tingkat / Ukuran Tempat Bekerja *</label>
-                                    <select class="form-select" aria-label="Default select example"
-                                        name="tingkat-ukuran-tempat-bekerja">
-                                        <option>Pilih Tingkat / Ukuran Tempat Bekerja</option>
-                                        <option value="a">Lokal</option>
-                                        <option value="b">Nasional</option>
-                                        <option value="c">Multinasional</option>
-                                    </select>
-                                </div>
-                                <div class="col-lg-4 col-md-6 col-sm-12 my-2">
-                                    <label class="form-label text-secondary">Posisi / Jabatan Pekerjaan *</label>
-                                    <select class="form-select" aria-label="Default select example"
-                                        name="posisi-jabatan-pekerjaan">
-                                        <option>Pilih Posisi / Jabatan Pekerjaan</option>
-                                        <option value="b">Direktur</option>
-                                        <option value="c">Kepala Unit</option>
-                                        <option value="d">Supervisor</option>
-                                        <option value="e">Staf</option>
-                                        <option value="f">Self Employed</option>
-                                    </select>
-                                </div>
-                                <div class="col-lg-4 col-md-6 col-sm-12 my-2">
-                                    <label class="form-label text-secondary">Detail Pekerjaan *</label>
-                                    <input type="text" id="formGroupExampleInput" name="detail-pekerjaan"
+                            <div class="col-lg-4 col-md-6 col-sm-12 my-2">
+                                <label class="form-label text-secondary">Tingkat / Ukuran Tempat Bekerja *</label>
+                                <select class="form-select" aria-label="Default select example"
+                                    name="tingkat-ukuran-tempat-bekerja">
+                                    <option>Pilih Tingkat / Ukuran Tempat Bekerja</option>
+                                    <option value="a">Lokal</option>
+                                    <option value="b">Nasional</option>
+                                    <option value="c">Multinasional</option>
+                                </select>
+                            </div>
+                            <div class="col-lg-4 col-md-6 col-sm-12 my-2">
+                                <label class="form-label text-secondary">Posisi / Jabatan Pekerjaan *</label>
+                                <select class="form-select" aria-label="Default select example"
+                                    name="posisi-jabatan-pekerjaan">
+                                    <option>Pilih Posisi / Jabatan Pekerjaan</option>
+                                    <option value="b">Direktur</option>
+                                    <option value="c">Kepala Unit</option>
+                                    <option value="d">Supervisor</option>
+                                    <option value="e">Staf</option>
+                                    <option value="f">Self Employed</option>
+                                </select>
+                            </div>
+                            <div class="col-lg-4 col-md-6 col-sm-12 my-2">
+                                <label class="form-label text-secondary">Detail Pekerjaan *</label>
+                                <input type="text" id="formGroupExampleInput" name="detail-pekerjaan"
+                                    class="form-control">
+                            </div>
+                            <div class="col-lg-4 col-md-6 col-sm-12 my-2">
+                                <label class="form-label text-secondary">Jumlah Pendapatan Perbulan *</label>
+                                <input type="number" id="formGroupExampleInput" name="jumlah-pendapatan-perbulan"
+                                    class="form-control">
+                            </div>
+                            <div class="col-lg-4 col-md-6 col-sm-12 my-2">
+                                <label class="form-label text-secondary">Kesesuaian Pekerjaan dengan Prodi *</label>
+                                <select class="form-select" aria-label="Default select example"
+                                    name="kesesuaian-pekerjaan-dengan-prodi">
+                                    <option>Pilih Kesesuaian Pekerjaan dengan Prodi</option>
+                                    <option value="a">Tinggi</option>
+                                    <option value="b">Sedang</option>
+                                    <option value="c">Rendah</option>
+                                </select>
+                            </div>
+                            <div class="col-lg-4 col-md-6 col-sm-12 my-2">
+                                <label class="form-label text-secondary">Tanggal Mulai Bekerja *</label>
+                                <input type="date" id="formGroupExampleInput" name="tanggal-mulai-bekerja"
+                                    class="form-control">
+                            </div>
+                            <div class="col-lg-4 col-md-6 col-sm-12 my-2">
+                                <label class="form-label text-secondary">Tanggal Akhir Kerja *</label>
+                                <input type="date" id="formGroupExampleInput" name="tanggal-akhir-kerja"
+                                    class="form-control">
+                            </div>
+                            <div class="col-lg-4 col-md-6 col-sm-12 my-2">
+                                <label class="form-label text-secondary">Provinsi *</label>
+                                <input type="text" id="formGroupExampleInput" name="provinsi" class="form-control">
+                            </div>
+                            <div class="col-lg-4 col-md-6 col-sm-12 my-2">
+                                <label class="form-label text-secondary">Kabupaten *</label>
+                                <input type="text" id="formGroupExampleInput" name="kabupaten" class="form-control">
+                            </div>
+                            <div class="col-lg-4 col-md-6 col-sm-12 my-2">
+                                <label class="form-label text-secondary">Foto/Bukti Telah Bekerja *</label>
+                                <div class="d-flex align-items-center">
+                                    <input type="file" id="formGroupExampleFile" name="fotobukti-telah-bekerja"
                                         class="form-control">
-                                </div>
-                                <div class="col-lg-4 col-md-6 col-sm-12 my-2">
-                                    <label class="form-label text-secondary">Jumlah Pendapatan Perbulan *</label>
-                                    <input type="number" id="formGroupExampleInput" name="jumlah-pendapatan-perbulan"
-                                        class="form-control">
-                                </div>
-                                <div class="col-lg-4 col-md-6 col-sm-12 my-2">
-                                    <label class="form-label text-secondary">Kesesuaian Pekerjaan dengan Prodi *</label>
-                                    <select class="form-select" aria-label="Default select example"
-                                        name="kesesuaian-pekerjaan-dengan-prodi">
-                                        <option>Pilih Kesesuaian Pekerjaan dengan Prodi</option>
-                                        <option value="a">Tinggi</option>
-                                        <option value="b">Sedang</option>
-                                        <option value="c">Rendah</option>
-                                    </select>
-                                </div>
-                                <div class="col-lg-4 col-md-6 col-sm-12 my-2">
-                                    <label class="form-label text-secondary">Tanggal Mulai Bekerja *</label>
-                                    <input type="date" id="formGroupExampleInput" name="tanggal-mulai-bekerja"
-                                        class="form-control">
-                                </div>
-                                <div class="col-lg-4 col-md-6 col-sm-12 my-2">
-                                    <label class="form-label text-secondary">Tanggal Akhir Kerja *</label>
-                                    <input type="date" id="formGroupExampleInput" name="tanggal-akhir-kerja"
-                                        class="form-control">
-                                </div>
-                                <div class="col-lg-4 col-md-6 col-sm-12 my-2">
-                                    <label class="form-label text-secondary">Provinsi *</label>
-                                    <input type="text" id="formGroupExampleInput" name="provinsi" class="form-control">
-                                </div>
-                                <div class="col-lg-4 col-md-6 col-sm-12 my-2">
-                                    <label class="form-label text-secondary">Kabupaten *</label>
-                                    <input type="text" id="formGroupExampleInput" name="kabupaten"
-                                        class="form-control">
-                                </div>
-                                <div class="col-lg-4 col-md-6 col-sm-12 my-2">
-                                    <label class="form-label text-secondary">Foto/Bukti Telah Bekerja *</label>
-                                    <div class="d-flex align-items-center">
-                                        <input type="file" id="formGroupExampleFile" name="fotobukti-telah-bekerja"
-                                            class="form-control">
-                                        <span class="ms-2" data-bs-toggle="popover" data-bs-title="Keterangan"
-                                            data-bs-content="File ini bisa berupa foto/screenshot yang membuktikan bahwa anda telah bekerja/berwirausaha, contoh: 1. Surat/Email Penerimaan Bekerja, 2. Surat Keputusan kontrak kerja, 3. Riwayat chat penerimaan /pemanggilan kerja, 4. Foto Tempat Berwirausaha, 5. Dan lain-lain">
-                                            <i class="bi bi-info-circle"></i>
-                                        </span>
-                                    </div>
+                                    <span class="ms-2" data-bs-toggle="popover" data-bs-title="Keterangan"
+                                        data-bs-content="File ini bisa berupa foto/screenshot yang membuktikan bahwa anda telah bekerja/berwirausaha, contoh: 1. Surat/Email Penerimaan Bekerja, 2. Surat Keputusan kontrak kerja, 3. Riwayat chat penerimaan /pemanggilan kerja, 4. Foto Tempat Berwirausaha, 5. Dan lain-lain">
+                                        <i class="bi bi-info-circle"></i>
+                                    </span>
                                 </div>
                             </div>
+                        </div>
 
-                            <hr>
-                            <div class="row justify-content-between" id="informasiPerusahaan">
-                                <span class="h4">Informasi Perusahaan</span>
-                                <div class="col-lg-4 col-md-6 col-sm-12 my-2">
-                                    <label class="form-label text-secondary">Nama Perusahaan *</label>
-                                    <input type="text" id="formGroupExampleInput" name="nama-perusahaan"
-                                        class="form-control">
-                                </div>
-                                <div class="col-lg-4 col-md-6 col-sm-12 my-2">
-                                    <label class="form-label text-secondary">Nama Atasan *</label>
-                                    <input type="text" id="formGroupExampleInput" name="nama-atasan"
-                                        class="form-control">
-                                </div>
-                                <div class="col-lg-4 col-md-6 col-sm-12 my-2">
-                                    <label class="form-label text-secondary">Posisi / Jabatan Atasan *</label>
-                                    <input type="text" id="formGroupExampleInput" name="posisi-jabatan-atasan"
-                                        class="form-control">
-                                </div>
-                                <div class="col-lg-4 col-md-6 col-sm-12 my-2">
-                                    <label class="form-label text-secondary">Nomor Telepon Atasan *</label>
-                                    <input type="text" id="formGroupExampleInput" name="nomor-telepon-atasan"
-                                        class="form-control">
-                                </div>
-                                <div class="col-lg-4 col-md-6 col-sm-12 my-2">
-                                    <label class="form-label text-secondary">Alamat Email Aktif Atasan *</label>
-                                    <input type="email" id="formGroupExampleInput" name="alamat-email-aktif-atasan"
-                                        class="form-control">
-                                </div>
+                        <hr>
+                        <div class="row justify-content-between" id="informasiPerusahaan">
+                            <span class="h4">Informasi Perusahaan</span>
+                            <div class="col-lg-4 col-md-6 col-sm-12 my-2">
+                                <label class="form-label text-secondary">Nama Perusahaan *</label>
+                                <input type="text" id="formGroupExampleInput" name="nama-perusahaan"
+                                    class="form-control">
                             </div>
+                            <div class="col-lg-4 col-md-6 col-sm-12 my-2">
+                                <label class="form-label text-secondary">Alamat Perusahaan *</label>
+                                <input type="text" id="formGroupExampleInput" name="alamat-perusahaan"
+                                    class="form-control">
+                            </div>
+                            <div class="col-lg-4 col-md-6 col-sm-12 my-2">
+                                <label class="form-label text-secondary">Nama Atasan *</label>
+                                <input type="text" id="formGroupExampleInput" name="nama-atasan"
+                                    class="form-control">
+                            </div>
+                            <div class="col-lg-4 col-md-6 col-sm-12 my-2">
+                                <label class="form-label text-secondary">Posisi / Jabatan Atasan *</label>
+                                <input type="text" id="formGroupExampleInput" name="posisi-jabatan-atasan"
+                                    class="form-control">
+                            </div>
+                            <div class="col-lg-4 col-md-6 col-sm-12 my-2">
+                                <label class="form-label text-secondary">Nomor Telepon Atasan *</label>
+                                <input type="text" id="formGroupExampleInput" name="nomor-telepon-atasan"
+                                    class="form-control">
+                            </div>
+                            <div class="col-lg-4 col-md-6 col-sm-12 my-2">
+                                <label class="form-label text-secondary">Alamat Email Aktif Atasan *</label>
+                                <input type="email" id="formGroupExampleInput" name="alamat-email-aktif-atasan"
+                                    class="form-control">
+                            </div>
+                        </div>
 
-                            <div class="row justify-content-end my-1" id="buttonGroup">
-                                <div class="col-lg-5 col-md-8 col-sm-12 text-end">
-                                    <a class="btn btn-secondary mx-1" href="/dashboard/perjalanan">
-                                        <i class="bi bi-arrow-left-circle"></i> Kembali
-                                    </a>
-                                    <button class="btn btn-success mx-1">
-                                        <i class="bi bi-file-earmark-check"></i> Simpan
-                                    </button>
-                                </div>
+                        <div class="row justify-content-end my-1" id="buttonGroup">
+                            <div class="col-lg-5 col-md-8 col-sm-12 text-end">
+                                <a class="btn btn-secondary mx-1" href="/dashboard/perjalanan">
+                                    <i class="bi bi-arrow-left-circle"></i> Kembali
+                                </a>
+                                <button class="btn btn-success mx-1">
+                                    <i class="bi bi-file-earmark-check"></i> Simpan
+                                </button>
                             </div>
+                        </div>
                     </form>
 
                 </div>
