a253d402ddb56742eafacd86d3bd73649b3220b3
Dimas Yudistira <alex.yudistira44@gmail.com>
2024-04-27 21:40:23 +0700

    minor fix on pekerjaan query

--- app/Http/Controllers/PerjalananKarirController.php
+++ app/Http/Controllers/PerjalananKarirController.php
@@ -15,13 +15,15 @@ class PerjalananKarirController extends Controller
 	public function index(){
 		$user_id = auth()->user()->id;
 
-		$pekerjaans = Pekerja::select(DB::raw("'pekerja' as tipe_kerja"), 'id', 'jabatan_pekerjaan', 'detail_pekerjaan', 'is_bekerja', 'tgl_mulai_kerja as tanggal_mulai', 'tgl_akhir_kerja as tanggal_akhir')
+		$pekerjaans = Pekerja::select(DB::raw("'pekerja' as tipe_kerja"), 'id', 'jabatan_pekerjaan', 'detail_pekerjaan', 'is_active', 'tgl_mulai_kerja as tanggal_mulai', 'tgl_akhir_kerja as tanggal_akhir')
 				->where('user_id', $user_id);
 
-		$wirausaha = Wirausaha::select(DB::raw("'wirausaha' as tipe_kerja"), 'id', 'jabatan_usaha', 'nama_usaha', DB::raw(1), 'tgl_mulai_usaha as tanggal_mulai', 'tgl_akhir_usaha as tanggal_akhir')
+		$wirausaha = Wirausaha::select(DB::raw("'wirausaha' as tipe_kerja"), 'id', DB::raw('1'), 'nama_usaha', 'is_active', 'tgl_mulai_usaha as tanggal_mulai', 'tgl_akhir_usaha as tanggal_akhir')
 				->where('user_id', $user_id);
 
 		$unioned = $pekerjaans->union($wirausaha)->orderBy('tanggal_mulai', 'asc')->get();
+
+		dd($unioned);
 
 		return view('dashboard.perjalanan-karir.index', [
 			'pekerjaans' => $unioned,
