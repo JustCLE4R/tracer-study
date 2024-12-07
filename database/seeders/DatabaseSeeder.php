a4b3c51fc67c47ed15aabe582ab1d64a640ecd02
Dimas Yudistira <alex.yudistira44@gmail.com>
2024-12-07 09:27:02 +0700

    ref: remove Question model, migration, and related seeding logic

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,8 +5,6 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 use App\Models\User;
-use App\Models\Career;
-use App\Models\Question;
 use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
 use Database\Seeders\AdminSeeder;
@@ -57,20 +55,6 @@ class DatabaseSeeder extends Seeder
 
     $this->call(AdminSeeder::class);
 
-    $filePath = storage_path('json/questionSeeder.json');
-
-    if(file_exists($filePath)){
-      $rawData = json_decode(file_get_contents($filePath), true);
-      foreach($rawData as $data){
-        Question::create([
-          'question' => $data['question'],
-          'qanswer' => json_encode($data['qanswer']),
-          'category' => $data['category'],
-          'type' => $data['type']
-        ]);
-      };
-    }
-
-    Career::factory(500)->create();
+    // Career::factory(500)->create();
   }
 }
